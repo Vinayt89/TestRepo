21857305f7966a913b817fd45de1bb12eaac3ed7
dhavalsdatar <Datar_Dhaval@hulft.com>
2017-08-17 15:26:06 +0900

    committed

--- src/main/java/com/test/hello/App.java
+++ src/main/java/com/test/hello/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "HELLOOOOOooOOOOOO!!" );
+        System.out.println( "HELLOOOOOO!!" );
     }
 }
