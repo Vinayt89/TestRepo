ed41aba3b92850b35255ee1f4c4199670d91462d
dhavalsdatar <Datar_Dhaval@hulft.com>
2017-10-11 08:34:39 +0900

    Test for reset soft

--- src/main/java/com/test/hello/TestClass.java
+++ src/main/java/com/test/hello/TestClass.java
@@ -3,6 +3,6 @@ package com.test.hello;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("HEllo from testClass!");
+		System.out.println("This is latest modification");
 	}
 }
