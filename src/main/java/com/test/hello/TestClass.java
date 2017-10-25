c230662c14c13573adea30189d55c4368746a641
Vinay Thakre <Vinay_Thakre@hulft.com>
2017-10-25 10:04:57 +0900

    updated!!!

--- src/main/java/com/test/hello/TestClass.java
+++ src/main/java/com/test/hello/TestClass.java
@@ -3,8 +3,6 @@ package com.test.hello;
 public class TestClass {
 
 	public static void main(String[] args) {
-	   String str = "hello!";
-	    
-	    str = (Integer) str;
+	  System.out.println("HelloooOO!!!!");
 	}
 }
