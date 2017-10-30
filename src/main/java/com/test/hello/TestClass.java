6e05ebe32c133674f55ca4ca314657dd27420ef4
Vinay Thakre <Vinay_Thakre@hulft.com>
2017-10-30 13:33:57 +0900

    Update TestClass.java

--- src/main/java/com/test/hello/TestClass.java
+++ src/main/java/com/test/hello/TestClass.java
@@ -3,6 +3,5 @@ package com.test.hello;
 public class TestClass {
 
 	public static void main(String[] args) {
-	  System.out.println("Jenkins Webhook Test2!!!!");
-	}
+	  System.out.println("Jenkins Webhook Test2!!!!")
 }
